777e4513216f5756c70605539a28d37da847b8ae
Nont49336 <46091888+Nont49336@users.noreply.github.com>
2023-02-01 14:10:42 +0700

    change: second commit

--- img_separator.py
+++ img_separator.py
@@ -1,7 +1,8 @@
 import os
 import cv2
+import numpy
 #  this is the program to extract the uncropped image to cropped image
 path = "./first_license"
 for root,dirs,files in os.walk(path):
-    for file in files:
-        print(os.path.join(root,file))
+    for file in files: 
+        cv2.imread(os.path.join(root,file)).shape
