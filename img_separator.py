cffe6a92d2b1670a7064eb698af1b3b808fe3d95
Nont49336 <46091888+Nont49336@users.noreply.github.com>
2023-02-03 00:12:20 +0700

    file separated

--- img_separator.py
+++ img_separator.py
@@ -1,8 +1,18 @@
 import os
 import cv2
-import numpy
+import shutil
 #  this is the program to extract the uncropped image to cropped image
 path = "./first_license"
+count = 0
 for root,dirs,files in os.walk(path):
     for file in files: 
-        cv2.imread(os.path.join(root,file)).shape
+        if (cv2.imread(os.path.join(root,file)).shape != ((486, 864, 3))): 
+            # print(cv2.imread(os.path.join(root,file)))
+            print(os.path.join(root,file))
+            shutil.move(os.path.join(root,file),os.path.join('first_processed',file))
+            count+=1
+            # print(os.path.join(root,'processed',file))
+        else:
+            print("not included")
+print("processed:",count)
+print("in processed folder:",len(os.listdir('first_processed')))
