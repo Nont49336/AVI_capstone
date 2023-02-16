17dba42304c1ef1d544b50bf32d4175e9fdfa5f7
Nont49336 <46091888+Nont49336@users.noreply.github.com>
2023-02-16 12:37:37 +0700

    add: function & clean the code

--- srcs/func.py
+++ srcs/func.py
@@ -18,47 +18,56 @@ import argparse
 # |--img1.jpg
 # |--img2.jpg
 # |--img3.jpg
+# return result and set 
+class Tesseract:
+    def __init__(self,psm=6,lang="tha",oem=3):
+        self.psm = psm
+        self.lang = lang
+        self.oem = oem
 
-def plate_analyzer(src):
+    def analyze(self,src):
     # single file handling
-    if (src.endswith(".jpg") or src.endswith(".png")):
-        dir = pytesseract.image_to_data(src, config=f'--psm 6',lang='tha',output_type=Output.DICT)
-        if (dir == None):
-            return print("can't detect plate and character")
+        if (src.endswith(".jpg") or src.endswith(".png")):
+            dir = pytesseract.image_to_data(src, config=f'--psm {self.psm} --oem {self.oem}',lang=self.lang,output_type=Output.DICT)
+            if (dir == None):
+                return print("can't detect plate and character")
+            else:
+                return print(dir['left'],dir['top'],dir['width'],dir['height'],dir['text'])
         else:
-            return print(dir['left'],dir['top'],dir['width'],dir['height'],dir['text'])
-    else:
-        #directory handling
-        for root,dirs,files in os.walk(src):
-            for file in files:
-                img = cv2.imread(os.path.join(root,file))
-        dir = pytesseract.image_to_data(img, config=f'--psm 4', lang='tha', output_type=Output.DICT)
-        text_marker(dir['left'],dir['top'],dir['width'],dir['height'],dir['text'])
-        n_boxes = len(dir['text'])
-        res_img = img
-        for i in range(n_boxes):
-            (x, y, w, h) = (dir['left'][i], dir['top'][i], dir['width'][i], dir['height'][i])
-            cv2.rectangle(res_img, (x, y), (x + w, y + h), (0, 0, 0), 3)
-            print(f"text{i}: {dir['text'][i]}")
-            res_img = Image.fromarray(res_img)
-            fontpath = "./angsana.ttc" # thai font
-            font = ImageFont.truetype(fontpath,16)  #h*2) #16)
-            draw = ImageDraw.Draw(res_img)
-            draw.text((x, y-h), dir['text'][i], font = font, fill = (0,0,255))
-            res_img = np.array(res_img)
-        cv2.imwrite(f'res_image_{i}.jpg', res_img)
+            #directory handling
+            for root,dirs,files in os.walk(src):
+                for file in files:
+                    img = cv2.imread(os.path.join(root,file))
+            dir = pytesseract.image_to_data(img, config=f'--psm 4', lang='tha', output_type=Output.DICT)
+            # text_roi = (dir['left'],dir['top'],dir['width'],dir['height'],dir['text'])
+            # text_marker()
+            # text_marker(dir)
+            n_boxes = len(dir['text'])
+            res_img = img
+            for i in range(n_boxes):
+                (x, y, w, h) = (dir['left'][i], dir['top'][i], dir['width'][i], dir['height'][i])
+                cv2.rectangle(res_img, (x, y), (x + w, y + h), (0, 0, 0), 3)
+                print(f"text{i}: {dir['text'][i]}")
+                res_img = Image.fromarray(res_img)
+                fontpath = "./angsana.ttc" # thai font
+                font = ImageFont.truetype(fontpath,16)  #h*2) #16)
+                draw = ImageDraw.Draw(res_img)
+                
+                draw.text((x, y-h), dir['text'][i], font = font, fill = (0,0,255))
+                res_img = np.array(res_img)
+            cv2.imwrite(f'res_image_{i}.jpg', res_img)
 
-    # full_text = "".join([str(i) for i in dir['text']])
-    # print(f"full text: {full_text}")
-    # cv2.imshow('res_image', res_img)
-    # cv2.waitKey(0)
-def text_marker(left,top,width,height,text=None):
-    
-    return None
+        # full_text = "".join([str(i) for i in dir['text']])
+        # print(f"full text: {full_text}")
+        # cv2.imshow('res_image', res_img)
+        # cv2.waitKey(0)
+    def char_marker(self,dir):
+        return None
 if __name__ == "__main__":
     # print("run mains")
     parser = argparse.ArgumentParser()
     parser.add_argument("-p","--path",help="path to file or folder")
     args = parser.parse_args()
     path = args.path
-    plate_analyzer(path)
+    detector = Tesseract(6,"tha",3)
+    detector.analyze(path)
---
+++ srcs/main.py
@@ -0,0 +1,6 @@
+from func import *
+
+
+folder_path = ""
+plate_analyzer(folder_path)
+
